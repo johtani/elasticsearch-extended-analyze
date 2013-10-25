ea03d49b5cc88aa4ff5ddaa086d7d8b6223f02e6
Jun Ohtani <johtani@gmail.com>
2013-10-25 23:16:23 +0900

    add file comment

--- src/test/java/org/elasticsearch/indices/extended/analyze/ExtendedAnalyzeActionTests.java
+++ src/test/java/org/elasticsearch/indices/extended/analyze/ExtendedAnalyzeActionTests.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2013 Jun Ohtani
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
 package org.elasticsearch.indices.extended.analyze;
 
 import org.elasticsearch.action.admin.indices.extended.analyze.ExtendedAnalyzeRequestBuilder;
@@ -54,8 +70,8 @@ public class ExtendedAnalyzeActionTests {
             "org.apache.lucene.analysis.tokenattributes.TermToBytesRefAttribute#bytes",
             "org.apache.lucene.analysis.tokenattributes.KeywordAttribute#keyword"};
         Map<String, Object> extendedAttribute = null;
-        for(int i=0;i<expectedAttributesKey.length;i++){
-            extendedAttribute = (Map<String, Object>)analyzeResponse.getTokens().get(2).getExtendedAttrbutes().get(i);
+        for (int i = 0; i < expectedAttributesKey.length; i++) {
+            extendedAttribute = (Map<String, Object>) analyzeResponse.getTokens().get(2).getExtendedAttrbutes().get(i);
             assertThat(extendedAttribute.size(), equalTo(1));
             assertThat(extendedAttribute.containsKey(expectedAttributesKey[i]), equalTo(true));
         }
